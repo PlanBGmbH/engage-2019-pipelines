9b3b57ed5b33fa6d75f87afbeee9905a17e0dcc1
NuKeeper <nukeeper@nukeeper.com>
2019-11-20 15:46:29 +0100

    Initial Checkin

---
+++ Setup.ps1
@@ -0,0 +1,16 @@
+## Setup Solution
+$repositoryName = "Engage2019.Pipelines"
+dotnet new sln -n Engage2019.Pipelines --force
+dotnet new func --name "$repositoryName" -o "./$repositoryName" -A v2 --force
+dotnet new http -na "$repositoryName.GetServiceInfo" -A Anonymous -n GetServiceInfo -o "./$repositoryName/GetServiceInfo" --force
+
+# Create Test Function
+dotnet new mstest --name "$repositoryName.UnitTests" -o "./$repositoryName.UnitTests" --force
+
+# Fill Solution
+dotnet sln "$repositoryName.sln" add "./$repositoryName/$repositoryName.csproj"
+dotnet sln "$repositoryName.sln" add "./$repositoryName.UnitTests/$repositoryName.UnitTests.csproj"
+
+## Create Pipeline
+az pipelines create --name engage2019-pipeline --repository DSpirit/engage2019-pipelines --repository-type github --yaml-path azure-pipelines.full.yml --branch master --organization https://dev.azure.com/wendelin --project dsdev --service-connection "849386f3-11ef-4659-ab84-4ec6538434622"
+
---
+++ templates/deploy-1.ps1
@@ -0,0 +1,13 @@
+Param(
+    [string]$location
+)
+
+$name = "engage2019"
+$stage = "test"#$(env:SYSTEM_STAGENAME)
+
+$group = (az group create --location $location --name "$name-$stage-$location") | ConvertFrom-Json
+Write-Host "Created ResourceGroup $($group.Name)"
+$storage = (az storage account create -n "$name$stage" -g $group.Name -l $location --sku Standard_LRS) | ConvertFrom-Json
+Write-Host "Created Storage $($storage.Name)"
+$app = az functionapp create --consumption-plan-location $location --name "$name-$stage-$location" --os-type Windows --resource-group $group.Name --runtime dotnet --storage-account $storage.Name 
+Write-Host "Created Function $($app.Name)"
