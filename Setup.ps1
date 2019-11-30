e40df6e8703ea0df3caaae571cc06ca1769b0a4e
NuKeeper <nukeeper@nukeeper.com>
2019-11-30 18:37:42 +0100

    Initialize Repository

--- Setup.ps1
+++ Setup.ps1
@@ -1,10 +1,14 @@
 # Variables
 $organization = "https://dev.azure.com/wendelin"
 $project = "engage2019"
+$servicePrincipalId = "e41a3a91-45b4-4bf7-ae65-c98755c345ca"
+$subscriptionId = "10b3c05a-71e7-4408-9130-fe16d0d569de"
+$subscriptionName = "Visual Studio Enterprise - MPN"
+$tenantId = "bebdf4c5-1357-4a2a-bc6e-85a882be76ae"
 
 ## Setup Solution
 $repositoryName = "Engage2019.Pipelines"
-dotnet new sln -n Engage2019.Pipelines --force
+dotnet new sln -n $repositoryName --force
 dotnet new func --name "$repositoryName" -o "./$repositoryName" -A v2 --force
 dotnet new http -na "$repositoryName.GetServiceInfo" -A Anonymous -n GetServiceInfo -o "./$repositoryName/GetServiceInfo" --force
 
@@ -23,7 +27,18 @@ $repository = az repos create --name $repositoryName --project $project --organi
 git remote add devops $repository.remoteUrl
 git add .
 git commit -m "Initialize Repository"
+
 git push devops master
 
+# Create Service Connection
+$serviceConnectionResult = az devops service-endpoint azurerm create --azure-rm-service-principal-id $servicePrincipalId `
+                                                                     --azure-rm-subscription-id $subscriptionId `
+                                                                     --azure-rm-subscription-name $subscriptionName `
+                                                                     --azure-rm-tenant-id $tenantId `
+                                                                     --name AzureRM
+
+$serviceConnection = $serviceConnectionResult | ConvertFrom-Json
+Write-Host $serviceConnection
+
 ## Create Pipeline
-az pipelines create --name engage2019-pipeline --repository $repositoryName --repository-type tfsgit --yaml-path azure-pipelines.yml --branch master --organization $organization --project $project
+az pipelines create --name engage2019-pipeline --repository $repositoryName --repository-type tfsgit --yaml-path azure-pipelines.yml --branch master --organization $organization --project $project --service-connection $serviceConnection.id
