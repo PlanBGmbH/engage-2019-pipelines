d6bb44f183af186ccb1bf371f3d434c2ec1493b8
NuKeeper <nukeeper@nukeeper.com>
2019-11-30 18:17:18 +0100

    Initialize Repository

---
+++ Engage2019.Pipelines.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Engage2019.Pipelines.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
